52abea08258e3b71c01746fe761d5b3b7f4f5d8d
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-04-10 12:55:39 -0300

    test cpmplete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Start</code> LAMMEP.
+          ACD<code>Start</code> LAMMEP.
         </p>
         <a
           className="App-link"
